16d799a440a13dda48db3370a24adc9c9ceb7ff5
Muhammaddedeandri74 <muhammaddedeandri98@gmail.com>
2022-05-13 14:22:31 +0700

    Update

--- views/Inventory/IngoingStatus.php
+++ views/Inventory/IngoingStatus.php
@@ -94,14 +94,14 @@
           <label for="" class="form-label fs-3 mb-3">Cetak & Download</label>
           <div class="me-3 mb-3">
             <p></p>
-            <button class="btn btn-light mt-3"><i class='bx bxs-download'>Download</i></button>
-            <button class="btn btn-light"><i class='bx bx-printer'>Cetak</i></button>
+            <a class="btn btn-light" id="btn_exportexcel"><i class='bx bxs-download'>Download</i></a>
+            <a class="btn btn-light" onclick="printdata()"><i class='bx bx-printer'>Cetak</i></a>
           </div>
         </div>
       </div>
       <div class="row">
         <label for="" class="form-label fs-4 mb-3">Item/Produk</label>
-        <table class="table table-bordered table-striped list-akses" id="table-user">
+        <table class="table table-bordered table-striped list-akses">
           <thead class="border-0">
             <tr>
               <td style="background:#1143d8;color:white;min-width: auto;">No</td>
@@ -145,11 +145,64 @@
           </tbody>
         </table>
       </div>
+      <pre id="result" style="display: none"></pre>
+      <div class="excel" id="excel">
+      </div>
+      <div class="data" id="data">
+        <table class="table table-striped table-hover">
+          <thead style="background: purple">
+            <tr>
+              <td style="border:solid;background:#1143d8;color:white;min-width: auto;">No</td>
+              <td style="border:solid;background:#1143d8;color:white;min-width: auto;">No. Transaksi</td>
+              <td style="border:solid;background:#1143d8;color:white;min-width: auto;">No. Purchase Order</td>
+              <td style="border:solid;background:#1143d8;color:white;min-width: auto;">Supplier</td>
+              <td style="border:solid;background:#1143d8;color:white;min-width: auto;">Nama Item</td>
+              <td style="border:solid;background:#1143d8;color:white;min-width: auto;">SKU</td>
+              <td style="border:solid;background:#1143d8;color:white;min-width: auto;">Gudang Penerima</td>
+              <td style="border:solid;background:#1143d8;color:white;min-width: auto;">Tangal InventoryIn</td>
+              <td style="border:solid;background:#1143d8;color:white;min-width: auto;">Tipe Ingoing</td>
+              <td style="border:solid;background:#1143d8;color:white;min-width: auto;">Tipe Item</td>
+              <td style="border:solid;background:#1143d8;color:white;min-width: auto;">QtyPo</td>
+              <td style="border:solid;background:#1143d8;color:white;min-width: auto;">QtyIn</td>
+              <td style="border:solid;background:#1143d8;color:white;min-width: auto;">Harga</td>
+              <td style="border:solid;background:#1143d8;color:white;min-width: auto;">Deskripsi</td>
+            </tr>
+          </thead>
+          <tbody id="printt">
+            <?php if ($data != "Not Found") : ?>
+              <?php $no = 1; ?>
+              <?php foreach ($data as $key) : ?>
+                <tr>
+                  <td style="border:solid;"><?php echo $no++; ?></td>
+                  <td style="border:solid;"><?php echo $key["codein"] ?></td>
+                  <td style="border:solid;"><?php echo $key["codepo"] ?></td>
+                  <td style="border:solid;"><?php echo $key["namesupp"] ?></td>
+                  <td style="border:solid;"><?php echo $key["nameitem"] ?></td>
+                  <td style="border:solid;"><?php echo $key["sku"] ?></td>
+                  <td style="border:solid;"><?php echo $key["namewarehouse"] ?></td>
+                  <td style="border:solid;"><?php echo $key["datein"] ?></td>
+                  <td style="border:solid;"><?php echo $key["typein"] ?></td>
+                  <td style="border:solid;"><?php echo $key["itemgroup"] ?></td>
+                  <td style="border:solid;"><?php echo $key["qtypodet"] ?></td>
+                  <td style="border:solid;"><?php echo $key["qtyindet"] ?></td>
+                  <td style="border:solid;"><?php echo number_format($key['price'], 0, '.', ',') ?></td>
+                  <td style="border:solid;"><?php echo $key["deskripsi"] ?></td>
+                </tr>
+              <?php endforeach ?>
+            <?php endif ?>
+          </tbody>
+        </table>
+      </div>
     </form>
   </div>
 </div>
-
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.5.3/jspdf.min.js"></script>
+<script type="text/javascript" src="https://html2canvas.hertzen.com/dist/html2canvas.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.14.5/xlsx.full.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script>
+  $('#data').hide();
+
   $(function() {
     $("#table-user").DataTable({
       "responsive": true,
@@ -166,5 +219,26 @@
       "autoWidth": false,
       "responsive": true,
     });
+  });
+
+  function printdata() {
+    var printContents = document.getElementById('data').innerHTML;
+    var originalContents = document.body.innerHTML;
+    document.body.innerHTML = printContents;
+    window.print();
+    document.body.innerHTML = originalContents;
+  }
+
+  $("#btn_exportexcel").click(function(e) {
+    let file = new Blob([$('.data').html()], {
+      type: "application/vnd.ms-excel"
+    });
+    console.log(file)
+    let url = URL.createObjectURL(file);
+    let a = $("<a />", {
+      href: url,
+      download: "InventoryIn.xls"
+    }).appendTo("body").get(0).click();
+    e.preventDefault();
   });
 </script>
